af3cadbcd79d7f957bd6f55e0884dabc55c0f4a7
Michael Cronk <michael.cronk@ibm.com>
2019-08-16 19:21:55 -0400

    Added docs

--- discoveryParser.js
+++ discoveryParser.js
@@ -2,6 +2,8 @@ const fs = require("graceful-fs");
 const rra = require("recursive-readdir-async");
 var path = require("path");
 var pdfUtil = require("pdf-to-text");
+var PDFParser = require("pdf2json");
+var mammoth = require("mammoth");
 const DiscoveryV1 = require("ibm-watson/discovery/v1");
 
 const discovery = new DiscoveryV1({
@@ -36,14 +38,32 @@ const get_PDF_List = async pathList => {
   var count = 0;
   pathList.forEach(function(file) {
     count++;
-    if (path.extname(file.fullname) == ".pdf") {
+    if (
+      path.extname(file.fullname) == ".pdf"
+      //|| path.extname(file.fullname) == ".docx"
+    ) {
       fileList.push(file.fullname);
     }
   });
   return fileList;
 };
 
-const parse_PDFs = async originalPath => {
+const get_Doc_List = async pathList => {
+  var fileList = [];
+  var count = 0;
+  pathList.forEach(function(file) {
+    count++;
+    if (
+      path.extname(file.fullname) == ".docx"
+      //|| path.extname(file.fullname) == ".docx"
+    ) {
+      fileList.push(file.fullname);
+    }
+  });
+  return fileList;
+};
+
+const parse_Files = async originalPath => {
   var isDirectory = await is_Directory(originalPath);
   var pathList = [];
   if (isDirectory[0] == true) {
@@ -67,7 +87,12 @@ const parse_PDFs = async originalPath => {
   }
 
   var pdfList = await get_PDF_List(pathList);
-  return pdfList;
+  var docList = await get_Doc_List(pathList);
+
+  console.log("pdfList length: " + pdfList.length);
+  console.log("docList length: " + docList.length);
+
+  return [pdfList, docList];
 
   //if directory read in everything into array
 
@@ -82,57 +107,103 @@ const add_doc_Discovery = async addDocumentParams => {
   return res_object;
 };
 
-const breakdown_PDFs = async pdfList => {
-  var pdfBufferList = {};
+const submit_String = async (data, filePath) => {
+  var splitList = [];
+  data = data.replace(/\s\s+/g, " ");
+  console.log("Length: " + data.length);
+  for (var i = 0, charsLength = data.length; i < charsLength; i += 50000) {
+    splitList.push(data.substring(i, i + 50000));
+    console.log("List Length: " + splitList.length);
+  }
+  var WDSQuery =
+    "<title>" + path.basename(filePath) + "</title>" + "<p>" + data + "</p>";
+  var add_document_params = {
+    environment_id: "27bd261d-c7ae-4312-9ae9-f9e821fb4675",
+    collection_id: "05d75643-6a7a-43d8-84e3-cc89f8f23b10",
+    file: WDSQuery
+  };
+  var discovery_ret = add_doc_Discovery(add_document_params);
+  return discovery_ret;
+};
 
+let pdfParser = new PDFParser();
+pdfParser.on("pdfParser_dataError", errData =>
+  console.error(errData.parserError)
+);
+pdfParser.on("pdfParser_dataReady", pdfData => {
+  //  console.log(pdfData.formImage.Pages[0]);
+  //  console.log(pdfData.formImage.Pages[0].Texts[0].R);
+});
+
+const breakdown_DOCs = async docList => {
+  await Promise.all(
+    docList.map(async docPath => {
+      mammoth
+        .extractRawText({ path: docPath })
+        .then(function(result) {
+          // var text = result.value; // The raw text
+          submit_String(result.value, docPath);
+        })
+        .done();
+    })
+  );
+  console.log("Done");
+};
+
+const breakdown_PDFs = async pdfList => {
   await Promise.all(
     pdfList.map(async pdfPath => {
       pdfUtil.pdfToText(pdfPath, function(err, data) {
         if (err) throw err;
-        data = data.replace(/\s\s+/g, " ");
 
-        console.log(data.length);
-        var splitList = [];
-        for (
-          var i = 0, charsLength = data.length;
-          i < charsLength;
-          i += 50000
-        ) {
-          splitList.push(data.substring(i, i + 50000));
-        }
+        submit_String(data, pdfPath);
 
-        console.log(splitList.length);
-        splitList.map(async stringChunk => {
-          var WDSQuery =
-            "<title>" +
-            path.basename(pdfPath) +
-            "</title>" +
-            "<p>" +
-            stringChunk +
-            "</p>";
-          var add_document_params = {
-            environment_id: "27bd261d-c7ae-4312-9ae9-f9e821fb4675",
-            collection_id: "2d40ea28-1f3d-4027-b259-39ca697c76cf",
-            file: WDSQuery
-          };
-          var discovery_ret = await add_doc_Discovery(add_document_params);
-        });
-
-        // pdfBufferList[pdfPath] = splitList;
-        // console.log(pdfBufferList);
+        // data = data.replace(/\s\s+/g, " ");
+        // for (
+        //   var i = 0, charsLength = data.length;
+        //   i < charsLength;
+        //   i += 50000
+        // ) {
+        //   splitList.push(data.substring(i, i + 50000));
+        //   //  console.log(splitList);
+        // }
+        // var WDSQuery =
+        //   "<title>" +
+        //   path.basename(pdfPath) +
+        //   "</title>" +
+        //   "<p>" +
+        //   data +
+        //   "</p>";
+        // var add_document_params = {
+        //   environment_id: "27bd261d-c7ae-4312-9ae9-f9e821fb4675",
+        //   collection_id: "05d75643-6a7a-43d8-84e3-cc89f8f23b10",
+        //   file: WDSQuery
+        // };
+        // var discovery_ret = add_doc_Discovery(add_document_params);
       });
     })
   );
-  console.log(pdfBufferList);
 };
 
 const main = async originalPath => {
-  var pdfList = await parse_PDFs(process.argv[2]);
+  var fileArray = await parse_Files(process.argv[2]);
+  console.log(fileArray);
+  var pdfList = fileArray[0];
+  var docList = fileArray[1];
+
   if (pdfList.length == 0) {
-    console.log("\nERROR: No PDFs found at path\n");
+    console.log("\nNo PDF files found at path\n");
   } else {
-    console.log("\n" + pdfList.length + " PDFs found, parsing...\n");
-    var parsedList = await breakdown_PDFs(pdfList);
+    console.log("\n" + pdfList.length + " PDF(s) found, parsing...\n");
+    breakdown_PDFs(pdfList);
+    // console.log("parsedList: " + parsedList);
+  }
+
+  if (docList.length == 0) {
+    console.log("\nERROR: No .docx files found at path\n");
+  } else {
+    console.log("\n" + docList.length + " .docx files found, parsing...\n");
+    breakdown_DOCs(docList);
   }
 };
 
